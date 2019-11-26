0eb89848ce41d6bf5dee8a2208cb5c5476fa5cb6
Jude <alex.jude@sap.com>
2019-11-26 16:47:12 +0530

    30 Days of Code

---
+++ 30DaysOfCode/Day 1: Data Types/DataTypes.py
@@ -0,0 +1,14 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+s_i = input()
+s_d = input()
+s_s = input()
+# Print the sum of both integer variables on a new line.
+print (i+int(s_i))
+# Print the sum of the double variables on a new line.
+print (d+float(s_d))
+# Concatenate and print the String variables on a new line
+print (s + s_s)
+# The 's' variable above should be printed first.
