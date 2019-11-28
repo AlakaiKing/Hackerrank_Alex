ee79ac871c62887a9c8ea928eebf44a621fa8eb0
Jude <alex.jude@sap.com>
2019-11-28 10:41:08 +0530

    Day 8: 30 DaysOfCode

---
+++ 30DaysOfCode/Day 8: Dictionaries and Maps/DictionariesAndMaps.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+ph = {}
+n = int(input())
+for _ in range(n):
+    inp = input().split()
+    ph[inp[0]] = inp[1]
+    inp = []
+
+query = sys.stdin.readline().strip()
+while query:
+    if query in ph:
+        print (query + "=" + ph[query])
+    else:
+        print ("Not found")
+    query = sys.stdin.readline().strip()
